f4cf8dae856541e4d4aef90f44a406f089925858
chunyiliu <86245624+chunyiliu@users.noreply.github.com>
2022-09-27 13:53:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -42,8 +44,11 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    message = event.message.text
+    if re.match("你是誰", message):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("才不告訴你勒~~"))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 import os
 if __name__ == "__main__":
